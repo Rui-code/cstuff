abcd8e0a96b3d616c4eb782afcfc7ca9d90123b2
Ruiran Oliveira <ruiranoliveirasantos@gmail.com>
2020-08-08 17:18:41 -0300

    fixing queue and making it circular

--- Structures/Queue.c
+++ Structures/Queue.c
@@ -2,76 +2,73 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define COUNT 100
+#define COUNT 6
 
 void enqueue (uint8_t num);
 void dequeue ();
-void print_queue ();
-void g_head (), g_back ();
+void print_head ();
 
 uint8_t fila[COUNT];
 uint8_t head = 0, back = 0;
 
 void main (void)
 {
-	enqueue(1);
-	enqueue(2);
-	enqueue(3);
-	enqueue(4);
-	enqueue(5);
-	enqueue(6);
-	print_queue();
-	g_head();
-	g_back();
-	dequeue();
-	print_queue();
-	g_head();
-	g_back();
-	enqueue(5);
-	enqueue(6);
-	print_queue();
-	dequeue();
-	dequeue();
-	g_head();
-	g_back();
-	print_queue();
+  enqueue(1);
+  enqueue(2);
+  enqueue(3);
+  print_head();
+  
+  puts("");
+  enqueue(4);
+  enqueue(5);
+  enqueue(6);
+  print_head();
+
+  dequeue();
+  dequeue();
+  dequeue();
+
+  enqueue(7);
+  
+  puts("");
+  print_head();
 }
 
 void enqueue (uint8_t num)
 {
-	fila[back++] = num;
-	
+  if (back == COUNT)
+  {
+    back = 0;
+    fila[back] = num;
+    back++;
+  }
+  else
+  {
+    fila[back] = num;
+    back++;
+  }
 }
 
-void print_queue (void)
+void print_head (void)
 {
-	register uint8_t i;
+  register uint8_t i;
 	
-	if (head == back)
-	{
-		printf("Fila Vazia.");
-	}
-	else
-	{
-		for (i = head; i < back; ++i)
-		{
-			printf("%d ", fila[i]);
-		}
-		printf("\n");
-	}
+  if (head == back)
+  {
+    printf("Fila Vazia.");
+  }
+  else
+  {
+      printf("%d ", fila[head]);
+  }
+    printf("\n");
 }
 
 void dequeue (void)
 {
-	++head;
-}
-
-void g_head ()
-{
-	printf("%d\n", fila[head]);
-}
-
-void g_back ()
-{
-	printf("%d\n", fila[back-1]);
+  if (head == COUNT)
+  {
+    head = -1;
+  }
+  ++head;
 }
